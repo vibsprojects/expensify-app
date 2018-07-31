359b82bbcd73e714bc39ecdab6bee8c6b3a5bebc
Vibhuti <iamseniorwebdev@gmail.com>
2018-07-31 21:42:58 +1000

    test case modify

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,18 +3,20 @@ import { shallow } from 'enzyme';
 import  { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
-test('should render AddExpensePage correctly',()=>{
-    const onSubmit=jest.fn();
-    const history= { psuh:jest.fn() };
-    const wrapper= shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
-    expect(wrapper).toMatchSnapshot();
-})
+let onSubmit, history , wrapper;
 
-test('should handle onSubmit',()=>{
-    const onSubmit=jest.fn();
-    const history= { push:jest.fn() };
-    const wrapper= shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+beforeEach(()=>{
+    onSubmit=jest.fn();
+    history= { push:jest.fn() };
+    wrapper= shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+});
+
+test('should render AddExpensePage correctly',()=>{ 
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle onSubmit',()=>{    
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith('/');
     expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
-})
+});
