31256367f138b2963a7da4ccdd672ea6ebf91fde
Vibhuti <iamseniorwebdev@gmail.com>
2018-08-26 14:44:35 +1000

    Public route

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = ({ startLogout }) =>{
     return (
         <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active" exact={true}>Create Expense</NavLink>
         {/*<NavLink to="/edit" activeClassName="is-active" exact={true}>Edit Expense</NavLink>*/}
         <NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,6 +8,7 @@ import EditExpensePage from '../components/EditExpensePage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -16,7 +17,7 @@ const AppRouter = () => {
         <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true}/>
+                <PublicRoute path="/" component={LoginPage} exact={true}/>
                 <PrivateRoute path="/dashboard" component={ExpenseDashBoardPage}/>
                 <PrivateRoute path="/create" component={AddExpensePage}/>
                 <PrivateRoute path="/edit/:id" component={EditExpensePage} />                
