162d613e4633d30da8f8421545509017cff9e1b5
Vibhuti <iamseniorwebdev@gmail.com>
2018-09-01 17:41:38 +1000

    more styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -57,12 +57,12 @@ class ExpenseForm extends React.Component {
         }
     };
     render(){
-     return (
-        <div> 
-            {this.state.error && <p>{this.state.error}</p>}
-            <form onSubmit={this.onSubmit}>
+     return (                   
+            <form className="form" onSubmit={this.onSubmit}>
+            {this.state.error && <p className="form__error">{this.state.error}</p>}
                 <input
                  type="text"
+                 className="text-input"
                  placeholder="Description"
                  autoFocus
                  value={this.state.description}
@@ -70,6 +70,7 @@ class ExpenseForm extends React.Component {
                 />
                 <input
                  type="text"
+                 className="text-input"
                  placeholder="Amount"                 
                  value={this.state.amount}
                  onChange={this.onAmountChange}
@@ -83,14 +84,16 @@ class ExpenseForm extends React.Component {
                 isOutsideRange={()=>false}
                 />
                 <textarea 
+                className="textarea"
                 value={this.state.note}
                 placeholder="Add a note for your expense (Optional)"
                 onChange={this.onNoteChange}
                 >
                 </textarea>
-                <button>Add Expense</button>
-            </form>        
-        </div>
+                <div>
+                    <button className="button">Save Expense</button>
+                </div>
+            </form>                
      )   
     }
 }
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -26,20 +26,29 @@ export class ExpenseListFilters extends React.Component {
   };
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          value={this.props.filters.text}
-          onChange={this.onTextChange}
-        />
-        <select
-          value={this.props.filters.sortBy}
-          onChange={this.onSortChange}
-        >
-          <option value="date">Date</option>
-          <option value="amount">Amount</option>
-        </select>
-        <DateRangePicker
+      <div className="content-container">
+      <div className="input-group">
+        <div className="input-group__item">
+            <input
+            className="text-input"
+            palceholder="Search expenses"
+            type="text"
+            value={this.props.filters.text}
+            onChange={this.onTextChange}
+            />
+        </div>
+        <div className="input-group__item">
+            <select
+            className="select"
+            value={this.props.filters.sortBy}
+            onChange={this.onSortChange}
+          >
+            <option value="date">Date</option>
+            <option value="amount">Amount</option>
+          </select>
+        </div>
+        <div className="input-group__item">
+          <DateRangePicker
           startDate={this.props.filters.startDate}
           endDate={this.props.filters.endDate}
           onDatesChange={this.onDatesChange}
@@ -49,6 +58,8 @@ export class ExpenseListFilters extends React.Component {
           numberOfMonths={1}
           isOutsideRange={() => false}
         />
+        </div>
+      </div>
       </div>
     );
   }
