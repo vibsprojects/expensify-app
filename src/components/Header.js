e72e011efd72f58be38566a8498c2caf95da1aa3
Vibhuti <iamseniorwebdev@gmail.com>
2018-09-01 12:57:36 +1000

    styles for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
 import  React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) =>{
     return (
-        <header>
-        <h1>Expensify</h1>
-        <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active" exact={true}>Create Expense</NavLink>
-        {/*<NavLink to="/edit" activeClassName="is-active" exact={true}>Edit Expense</NavLink>*/}
-        <NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>
-        <button onClick={startLogout}>Logout</button>
+        <header className="header">     
+        <div className="content-container">
+            <div className="header__content">
+                <Link to="/dashboard" className="header__title">
+                    <h1>Expensify</h1>
+                </Link>
+                {/*<NavLink to="/create" activeClassName="is-active" exact={true}>Create Expense</NavLink>*/}
+                {/*<NavLink to="/edit" activeClassName="is-active" exact={true}>Edit Expense</NavLink>*/}
+                {/*<NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>*/}
+                <button onClick={startLogout} className="button button--link">Logout</button>
+            </div>
+        </div>        
         </header>
     );
 }
